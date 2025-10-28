eef3f799fb9e0b8066e10a91c05a5a04ae56b136
Santiago Muscolo <santiago.muscolo@ingenia.la>
2025-10-28 20:48:47 -0300

    leaf-similar-trees

---
+++ easy/leaf-similar-trees.ts
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function leafSimilar(root1: TreeNode | null, root2: TreeNode | null): boolean {
+  function getLeaves(node: TreeNode | null) {
+    if (!node) return [];
+    if (!node.left && !node.right) return [node.val];
+
+    return [...getLeaves(node.left), ...getLeaves(node.right)];
+  }
+
+  return JSON.stringify(getLeaves(root1)) === JSON.stringify(getLeaves(root2));
+}
